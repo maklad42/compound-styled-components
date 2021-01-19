5e250f329f63b45a74993eb15dd7b42618021534
maklad42 <maklad42+git@gmail.com>
2021-01-19 23:22:53 +0900

    Add image files. 210119

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import classNames from 'classnames';
+import { Container, Body, Title, Text, Image, Button } from './styles/Card';
 
 export function Card({ classes, children, ...restProps }) {
-  return <Container className={classNames('card', classes, ...restProps)}>{children}</Container>;
+  return (
+    <Container className={classNames('card', classes)} {...restProps}>
+      {children}
+    </Container>
+  );
 }
