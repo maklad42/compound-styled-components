18d46ece0dd8ffd8a0cf4ccc11c94cfa393d6967
maklad42 <maklad42+git@gmail.com>
2021-01-20 23:10:31 +0900

    Few more updates. 210120

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from './components';
+import { Card } from './components/Card';
 import movies from './data';
 
 function App() {
@@ -8,6 +8,10 @@ function App() {
       {movies.map((movie) => (
         <Card className="mr">
           <Card.Image src={movie.image} alt={movie.title} />
+          <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.desc}</Card.Text>
+          <Card.Button>{movie.ctaText}</Card.Button>
         </Card>
       ))}
     </main>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import classNames from 'classnames';
 import { Container, Body, Title, Text, Image, Button } from './styles/Card';
@@ -9,3 +10,41 @@ export function Card({ classes, children, ...restProps }) {
     </Container>
   );
 }
+
+Card.Body = function CardBody({ classes, children, ...restProps }) {
+  return (
+    <Body className={classNames('card__body', classes)} {...restProps}>
+      {children}
+    </Body>
+  );
+};
+
+Card.Title = function CardTitle({ classes, children, ...restProps }) {
+  return (
+    <Title className={classNames('card__title', classes)} {...restProps}>
+      {children}
+    </Title>
+  );
+};
+
+Card.Text = function CardText({ classes, children, ...restProps }) {
+  return (
+    <Text className={classNames('card__text', classes)} {...restProps}>
+      {children}
+    </Text>
+  );
+};
+
+Card.Button = function CardButton({ classes, children, ...restProps }) {
+  return (
+    <Button className={classNames('card__button', classes)} {...restProps}>
+      {children}
+    </Button>
+  );
+};
+
+Card.Image = function CardImage({ src, alt, classes, ...restProps }) {
+  return (
+    <Image src={src} alt={alt} className={classNames('card__image', classes)} {...restProps} />
+  );
+};
