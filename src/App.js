f9d13ad3c7f6334dd267f1d0eacb34c4ddf6b6d2
maklad42 <maklad42+git@gmail.com>
2021-01-21 21:22:23 +0900

    Completed the code and removed unecessary files. 210121

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.scss';
 import { Card } from './components/Card';
 import movies from './data';
 
@@ -6,12 +7,13 @@ function App() {
   return (
     <main>
       {movies.map((movie) => (
-        <Card className="mr">
+        <Card className="mr" key={`${movie.id}`}>
           <Card.Image src={movie.image} alt={movie.title} />
           <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Card.Text>{movie.desc}</Card.Text>
-          <Card.Button>{movie.ctaText}</Card.Button>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.desc}</Card.Text>
+            <Card.Button>{movie.ctaText}</Card.Button>
+          </Card.Body>
         </Card>
       ))}
     </main>
